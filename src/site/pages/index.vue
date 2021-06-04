f7b7e37d24cf084dc0d4f6c896d01d5f74084207
Simon <33012273+5E7EN@users.noreply.github.com>
2021-06-03 20:50:22 -0400

    Cleanup Homepage

--- src/site/pages/index.vue
+++ src/site/pages/index.vue
@@ -1,27 +1,19 @@
 <template>
 	<div>
-		<div class="logoContainer">
-			<Logo />
-		</div>
-		<div class="leftSpacer">
+		<div class="root">
 			<div class="mainBlock">
 				<div>
-					<h4>Blazing fast file uploader. For real.</h4>
-					<p>
-						A <strong>modern</strong> and self-hosted file upload service that can handle anything you throw at it.
-					</p>
-					<p>
-						With a fast API, chunked file uploads out of the box, beautiful masonry-style file manager and both individual and album sharing capabilities, this little tool was crafted with the best user experience in mind.<br>
-					</p>
+					<h4>5E7EN File Uploader</h4>
 					<div class="mt4" />
 					<Uploader v-if="config.publicMode || (!config.publicMode && loggedIn)" />
 					<div
 						v-else
-						class="has-text-right is-size-4">
-						This site has disabled public uploads. You need an account.
+						class="is-size-4">
+						Powered by
+						<a href="https://github.com/WeebDev/chibisafe" target="_blank">
+							chibisafe
+						</a>
 					</div>
-
-					<Links />
 				</div>
 			</div>
 		</div>
@@ -30,16 +22,12 @@
 <script>
 import { mapState, mapGetters } from 'vuex';
 
-import Logo from '~/components/logo/Logo.vue';
 import Uploader from '~/components/uploader/Uploader.vue';
-import Links from '~/components/home/links/Links.vue';
 
 export default {
 	name: 'Home',
 	components: {
-		Logo,
-		Uploader,
-		Links
+		Uploader
 	},
 	data() {
 		return { albums: [] };
@@ -56,20 +44,13 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-	.logoContainer {
-		position: fixed;
-		top: calc(45% - 188px);
-		left: calc(22% - 117px);
-	}
-	.leftSpacer {
+	.root {
 		width: 56%;
-		margin-left: auto;
 		position: relative;
 		.mainBlock {
 			height: calc(100vh - 52px);
 			position: relative;
 			margin: 0 5rem;
-			text-align: right;
 			> div {
 				position: absolute;
 				top: 25%;
@@ -85,13 +66,7 @@ export default {
 	}
 
 	@media (max-width: 1025px) {
-		.logoContainer {
-			position: relative;
-			top: 0;
-			left: 0;
-			text-align: center;
-		}
-		.leftSpacer {
+		.root {
 			width: 100%;
 			.mainBlock {
 				height: auto;
